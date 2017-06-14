b4c3360dd53055db2499778e436c3c08246e42f8
mkhyperlink <mkhyperlink@gmail.com>
2017-06-14 16:37:32 +0800

    Change get credentials flow and save it to local.

--- googleDriveAccess.py
+++ googleDriveAccess.py
@@ -11,7 +11,10 @@ import httplib2
 import apiclient.discovery
 import apiclient.http
 import oauth2client.client
+from oauth2client import tools
+from oauth2client.file import Storage
 import webbrowser
+import os
 
 import fileOperate
 
@@ -47,14 +50,53 @@ MIMETYPE = 'text/plain'
 TITLE = 'My New Text Document'
 DESCRIPTION = 'A shiny new text document about hello world.'
 
-# Perform OAuth2.0 authorization flow.
-flow = oauth2client.client.flow_from_clientsecrets(CLIENT_SECRETS, OAUTH2_SCOPE)
-flow.redirect_uri = oauth2client.client.OOB_CALLBACK_URN
-authorize_url = flow.step1_get_authorize_url()
-# print 'Go to the following link in your browser: ' + authorize_url
-webbrowser.open_new_tab(authorize_url)
-code = raw_input('Enter verification code: ').strip()
-credentials = flow.step2_exchange(code)
+APPLICATION_NAME = 'Google Drive Access'
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+
+    try:
+        import argparse
+        flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+    except ImportError:
+        flags = None
+
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'googleDriveAccess.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = oauth2client.client.flow_from_clientsecrets(CLIENT_SECRETS, OAUTH2_SCOPE)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def get_credentials_by_input():
+	# Perform OAuth2.0 authorization flow.
+	flow = oauth2client.client.flow_from_clientsecrets(CLIENT_SECRETS, OAUTH2_SCOPE)
+	flow.redirect_uri = oauth2client.client.OOB_CALLBACK_URN
+	authorize_url = flow.step1_get_authorize_url()
+	# print 'Go to the following link in your browser: ' + authorize_url
+	webbrowser.open_new_tab(authorize_url)
+	code = raw_input('Enter verification code: ').strip()
+	return flow.step2_exchange(code)
+
+credentials = get_credentials()
 
 # Create an authorized Drive API client.
 http = httplib2.Http()
